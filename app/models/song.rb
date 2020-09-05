af25bfbb53438ce2acb167357d317778a8ac884d
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-05 12:13:05 -0700

    add comments explaining when to not mass assign and update

--- app/models/song.rb
+++ app/models/song.rb
@@ -12,24 +12,26 @@ class Song < ApplicationRecord
     # accepts_nested_attributes_for :genre, reject_if: :all_blank # use this line for generic nested attribute writer method
 
     def artist_attributes=(artist_attributes)
-        if artist_attributes[:name] != ""
-            # self.build_artist(artist_attributes) # use this line if not checking for duplicates
-            self.artist = Artist.find_or_create_by(name: artist_attributes[:name])
-            self.artist.update(artist_attributes)
+        # self.build_artist(artist_attributes) # use this line if not checking for duplicates
+        if artist_attributes[:name].present?
+            self.artist = Artist.find_or_create_by(artist_attributes)
+            # self.artist = Artist.find_or_create_by(name: artist_attributes[:name]) #only necessary if artist has attributes other than :name
+            # self.artist.update(artist_attributes) #only necessary if artist has attributes other than :name
         end
     end
 
     def genre_attributes=(genre_attributes)
-        if genre_attributes[:name] != ""
-            # self.build_genre(genre_attributes) # use this line if not checking for duplicates
-            self.genre = Genre.find_or_create_by(name: genre_attributes[:name])
-            self.genre.update(genre_attributes)
+        # self.build_genre(genre_attributes) # use this line if not checking for duplicates
+        if genre_attributes[:name].present?
+            self.genre = Genre.find_or_create_by(genre_attributes)
+            # self.genre = Genre.find_or_create_by(name: genre_attributes[:name]) #only necessary if genre has attributes other than :name
+            # self.genre.update(genre_attributes) #only necessary if genre has attributes other than :name
         end
     end
 
     def notes_attributes=(notes_attributes)
         notes_attributes.values.each do |note_attributes| 
-            if note_attributes[:content] != ""
+            if note_attributes[:content].present?
                 self.notes.build(note_attributes)
             end
         end
