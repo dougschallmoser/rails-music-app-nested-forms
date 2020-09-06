c156cd763c8ace12a09ffeca44f6faa63fa00f88
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-06 11:48:08 -0700

    add a new index page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :artists
   resources :songs
 
-  root 'songs#index'
+  root 'application#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
